f97fc8fda5680fe32de094bf0a013b148f1e50b1
Antoine Place <antoineplace2212@gmail.com>
2019-05-22 14:33:54 +0200

    backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  Restaurant.create!(name: "restaurant #{n}", address: "#{n} rue de machin", category: 'chinese')
+end
+
+Restaurant.all.each do |r|
+  10.times do |n|
+    Review.create!(content: "c'est nul #{n}", rating: n % 5, restaurant: r)
+  end
+end
